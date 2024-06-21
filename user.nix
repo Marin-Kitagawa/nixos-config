970f149fd5e7b126afe9327f5db04ef6a40fa737
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2024-06-22 02:49:55 +0530

    Initial NixOS Setup

---
+++ user.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, ... }:
+let
+	unstable = import <unstable> { config = {allowUnfree = true; }; };
+in {
+	environment.systemPackages = with pkgs; [
+		unstable.autojump
+		unstable.bat
+		unstable.brave
+		unstable.chezmoi
+		unstable.cmake
+		unstable.dust
+		unstable.dutree
+		unstable.eza
+		unstable.fastfetch
+		unstable.fzf
+		unstable.gcc
+		unstable.git
+		unstable.git-extras
+		unstable.gnome-extension-manager
+		unstable.gnome.gnome-themes-extra
+		unstable.gnome.gnome-tweaks
+		unstable.gnomeExtensions.gsconnect
+		unstable.jq
+		unstable.lua
+		unstable.neovim
+		unstable.nushell
+		unstable.onefetch
+		unstable.p7zip
+		unstable.qbittorrent
+		unstable.ripgrep
+		unstable.ripgrep-all
+		unstable.syncthing
+		unstable.tldr
+		unstable.vivaldi
+		unstable.vivaldi-ffmpeg-codecs
+		unstable.vlc
+		unstable.wezterm
+		unstable.whitesur-cursors
+		unstable.whitesur-gtk-theme
+		unstable.whitesur-icon-theme
+		unstable.wl-clipboard
+		unstable.xcp
+		unstable.zig
+		unstable.zsh
+	];
+}
