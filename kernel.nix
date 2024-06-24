9fcaa219c90bb2fce8df7b066d36ba9a438851b2
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2024-06-25 00:34:17 +0530

    Added GRUB theme configuration

--- kernel.nix
+++ kernel.nix
@@ -13,6 +13,14 @@
         enable = true;
         device = "nodev";
         efiSupport = true;
+	theme = "${
+	  (pkgs.fetchFromGitHub {
+	    owner = "Marin-Kitagawa";
+	    repo = "nixos-themes";
+	    rev = "b1e3ddb67c7d5b06858d28b3e82da517ba66e3f2";
+	    sha256 = "fy3YlOufmQT+mM8EnmzaVUTXVuG2MBdbIVmdKrmJHDI=";
+	  })
+	}/grub/RuanMei";
       };
       systemd-boot.enable = false;
       efi.canTouchEfiVariables = true;
