0c2e432759c7966a9e62c3748dceb44a4c708217
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2024-07-14 00:24:03 +0530

    Added support for accelerated video playback

--- nvidia.nix
+++ nvidia.nix
@@ -3,7 +3,13 @@
   # Enable OpenGL
   hardware.graphics = {
     enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      libvdpau-va-gl
+    ];
   };
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Force intel-media-driver
 
   services.xserver.videoDrivers = ["nvidia"];
 
