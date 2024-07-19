834e20ea54c7ba086caec1e1f8ed949d3bd21dc2
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2024-07-19 22:22:10 +0530

    Added support for declarative installation of Flatpaks

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     stylix.url = "github:danth/stylix";
+    nix-flatpak.url = "github:gmodena/nix-flatpak";
   };
 
   outputs = { nixpkgs, ... }@inputs:
@@ -22,6 +23,7 @@
       };
       modules = [
         inputs.stylix.nixosModules.stylix
+        inputs.nix-flatpak.nixosModules.nix-flatpak
         ./configuration.nix
       ];
     };
