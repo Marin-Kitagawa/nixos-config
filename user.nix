a235896185aedfc1cf0112faa87ba8a64a4276fe
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2024-07-19 21:21:02 +0530

    Added some packages for wayland

--- user.nix
+++ user.nix
@@ -47,9 +47,14 @@ in {
 		unstable.kdePackages.dolphin-plugins
 		unstable.kdePackages.kdeconnect-kde
 		unstable.kdePackages.kiconthemes
+		unstable.kdePackages.kwayland
+		unstable.kdePackages.kwayland-integration
+		unstable.kdePackages.layer-shell-qt
 		unstable.kdePackages.plasma-integration
 		unstable.kdePackages.qt6ct
 		unstable.kdePackages.qtstyleplugin-kvantum
+		unstable.kdePackages.qtwayland
+		unstable.kdePackages.wayqt
 		unstable.keepassxc
 		unstable.libsForQt5.qt5ct
 		unstable.libsForQt5.qtstyleplugin-kvantum
