95581a14ca9f94d51be52432de0d5c744bc8d931
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2024-07-23 13:25:37 +0530

    Simplified the config and added Niv
    Niv is the Nix dependency manager

--- user.nix
+++ user.nix
@@ -1,7 +1,5 @@
 { config, pkgs, ... }:
-let
-	unstable = import <nixos> { config = {allowUnfree = true; }; };
-in {
+{
 	imports = [
 	];
 	nixpkgs.overlays =
@@ -15,86 +13,87 @@ in {
 	];
 	programs.firefox.package = pkgs.latest.firefox-nightly-bin;
 	environment.systemPackages = with pkgs; [
-		(lib.hiPrio unstable.gcc)
-		(lib.lowPrio unstable.clang)
-		unstable.appimage-run
-		unstable.autojump
-		unstable.bat
-		unstable.brave
-		unstable.btop
-		unstable.bun
-		unstable.catppuccin-kde
-		unstable.chezmoi
-		unstable.cmake
-		unstable.colloid-kde
-		unstable.delta
-		unstable.docker
-		unstable.docker-compose
-		unstable.dust
-		unstable.dutree
-		unstable.eza
-		unstable.fastfetch
-		unstable.fsearch
-		unstable.fzf
-		unstable.gh
-		unstable.git
-		unstable.git-credential-oauth
-		unstable.git-extras
-		unstable.gitFull
-		unstable.gitkraken
-		unstable.gnumake
-		unstable.gnupg
-		unstable.jq
-		unstable.kde-rounded-corners
-		unstable.kdePackages.dolphin-plugins
-		unstable.kdePackages.kdeconnect-kde
-		unstable.kdePackages.kiconthemes
-		unstable.kdePackages.plasma-integration
-		unstable.kdePackages.qt6ct
-		unstable.kdePackages.qtstyleplugin-kvantum
-		unstable.keepassxc
-		unstable.libsForQt5.qt5ct
-		unstable.libsForQt5.qtstyleplugin-kvantum
-		unstable.lua
-		unstable.neovim
-		unstable.nix-prefetch-github
-		unstable.nodePackages_latest.nodejs
-		unstable.nurl
-		unstable.nushell
-		unstable.onefetch
-		unstable.onlyoffice-bin_latest
-		unstable.p7zip
-		unstable.pavucontrol
-		unstable.pinentry-gtk2
-		unstable.podman
-		unstable.poetry
-		unstable.python3
-		unstable.python312Packages.pip
-		unstable.qbittorrent
-		unstable.reversal-icon-theme
-		unstable.ripgrep
-		unstable.ripgrep-all
-		unstable.sqlite
-		unstable.stacer
-		unstable.steam-run
-		unstable.stilo-themes
-		unstable.sweet-nova
-		unstable.syncthing
-		unstable.themix-gui
-		unstable.tldr
-		unstable.tokyonight-gtk-theme
-		unstable.vlc
-		unstable.wezterm
-		unstable.whitesur-cursors
-		unstable.whitesur-gtk-theme
-		unstable.whitesur-icon-theme
-		unstable.wl-clipboard
-		unstable.xcp
-		unstable.yarn-berry
-		unstable.yazi
-		unstable.yq
-		unstable.zellij
-		unstable.zig
-		unstable.zsh
+		(lib.hiPrio gcc)
+		(lib.lowPrio clang)
+		appimage-run
+		autojump
+		bat
+		brave
+		btop
+		bun
+		catppuccin-kde
+		chezmoi
+		cmake
+		colloid-kde
+		delta
+		docker
+		docker-compose
+		dust
+		dutree
+		eza
+		fastfetch
+		fsearch
+		fzf
+		gh
+		git
+		git-credential-oauth
+		git-extras
+		gitFull
+		gitkraken
+		gnumake
+		gnupg
+		jq
+		kde-rounded-corners
+		kdePackages.dolphin-plugins
+		kdePackages.kdeconnect-kde
+		kdePackages.kiconthemes
+		kdePackages.plasma-integration
+		kdePackages.qt6ct
+		kdePackages.qtstyleplugin-kvantum
+		keepassxc
+		libsForQt5.qt5ct
+		libsForQt5.qtstyleplugin-kvantum
+		lua
+		neovim
+		nix-prefetch-github
+		niv
+		nodePackages_latest.nodejs
+		nurl
+		nushell
+		onefetch
+		onlyoffice-bin_latest
+		p7zip
+		pavucontrol
+		pinentry-gtk2
+		podman
+		poetry
+		python3
+		python312Packages.pip
+		qbittorrent
+		reversal-icon-theme
+		ripgrep
+		ripgrep-all
+		sqlite
+		stacer
+		steam-run
+		stilo-themes
+		sweet-nova
+		syncthing
+		themix-gui
+		tldr
+		tokyonight-gtk-theme
+		vlc
+		wezterm
+		whitesur-cursors
+		whitesur-gtk-theme
+		whitesur-icon-theme
+		wl-clipboard
+		xcp
+		yarn-berry
+		yazi
+		yq
+		zellij
+		zig
+		zsh
 	];
 }
