5633f6bcad5c0c2a71f7ad5b5d507cf891ca3579
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2024-10-09 01:02:22 +0530

    Updated flakes, `niv` sources and removed Firefox nightly

--- user.nix
+++ user.nix
@@ -2,16 +2,6 @@
 {
 	imports = [
 	];
-	nixpkgs.overlays =
-	let
-	  # Change this to a rev sha to pin
-	  moz-rev = "master";
-	  moz-url = builtins.fetchTarball { url = "https://github.com/mozilla/nixpkgs-mozilla/archive/${moz-rev}.tar.gz";};
-	  nightlyOverlay = (import "${moz-url}/firefox-overlay.nix");
-	in [
-	  nightlyOverlay
-	];
-	programs.firefox.package = pkgs.latest.firefox-nightly-bin;
 	environment.systemPackages = with pkgs; [
 		(lib.hiPrio gcc)
 		(lib.lowPrio clang)
