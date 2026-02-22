1036edba36a6f9bf25a77d539f10a0c67ab276b2
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2026-02-22 08:45:45 +0530

    refactor: Formatted files using Alejandra

--- packages/web_internet/download_managers.nix
+++ packages/web_internet/download_managers.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     kdePackages.kget
     uget
@@ -7,10 +6,8 @@
   ];
 
   # Explicitly link the manifest to the location Brave expects on Linux
-  environment.etc."brave/native-messaging-hosts/com.ugetdm.chrome.json".source =
-    "${pkgs.uget-integrator}/etc/chromium/native-messaging-hosts/com.ugetdm.chrome.json";
+  environment.etc."brave/native-messaging-hosts/com.ugetdm.chrome.json".source = "${pkgs.uget-integrator}/etc/chromium/native-messaging-hosts/com.ugetdm.chrome.json";
 
   # Also link it for standard Chromium/Chrome just in case
-  environment.etc."chromium/native-messaging-hosts/com.ugetdm.chrome.json".source =
-    "${pkgs.uget-integrator}/etc/chromium/native-messaging-hosts/com.ugetdm.chrome.json";
+  environment.etc."chromium/native-messaging-hosts/com.ugetdm.chrome.json".source = "${pkgs.uget-integrator}/etc/chromium/native-messaging-hosts/com.ugetdm.chrome.json";
 }
